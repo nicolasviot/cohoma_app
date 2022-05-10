987fe150e51a5f488f0db4930f6d73d2a4803e30
nicolas viot <nicolasviot2@gmail.com>
2022-05-10 11:21:47 +0200

    update compilation

--- srcs.mk
+++ srcs.mk
@@ -14,13 +14,13 @@ srcs_sma ?= src/ClockComponent.sma src/Reticule.sma src/Strip.sma src/Slider.sma
             src/ItineraryDescriptor.sma src/ItineraryPannel.sma src/RightPannel.sma src/RosManager.sma \
             src/Vehicule.sma src/UpperLeftMenu.sma src/StripContainer.sma src/main.sma
 # or
-#srcs_sma := $(shell find $(src_dir) -name "*.sma")
+#srcs_sma := $(shell find $(src_dir) -name "*.s")
 
 # native sources
 srcs_other ?= src/cpp/coords-utils.cpp src/cpp/map_move.cpp src/cpp/tiles_manager.cpp \
-             src/ros_node.cpp\
+             src/ros_node.cpp #src/ros_node_inst.cpp
 
-build/src/ros_node.o: CXXFLAGS+=-fno-implicit-templates
+#build/src/ros_node.o: CXXFLAGS+=-fno-implicit-templates
 
 # or
 #srcs_other := $(shell find $(src_dir) -name "*.cpp")
@@ -36,10 +36,22 @@ ros_include_path := $(ros_install_path)/include
 ros_lib_path := $(ros_install_path)/lib
 
 ros_libs := $(shell ls $(ros_lib_path)/lib*.so 2>/dev/null | xargs echo)
+
+#ros_libs := $(filter-out $(ros_lib_path)/librmw_cyclonedds_cpp.so, $(ros_libs))
+#ros_libs := $(filter-out $(ros_lib_path)/librosbag2_storage_default_plugins.so, $(ros_libs))
+#ros_libs := $(filter-out $(ros_lib_path)/librosbag2_storage.so, $(ros_libs))
+ros_libs := $(filter-out $(ros_lib_path)/librviz_common.so, $(ros_libs))
+ros_libs := $(filter-out $(ros_lib_path)/librviz_rendering.so, $(ros_libs))
+ros_libs := $(filter-out $(ros_lib_path)/librviz_default_plugins.so, $(ros_libs))
+
 ros_libs := $(patsubst $(ros_lib_path)/lib%.so,-l%,$(ros_libs))
 
 ros_x86_libs := $(shell ls $(ros_lib_path)/x86_64-linux-gnu/lib*.so 2>/dev/null | xargs echo)
-#ros_libs := $(filter-out $(ros_lib_path)/librmw_cyclonedds_cpp.so, $(ros_libs))
+
+truc:
+	echo $(ros_libs)
+
+
 # Nico pourrais-tu essayer ceci vv à la place de cela ^^ stp?
 
 # rclcpp_lib_deps := $(shell ldd $(ros_lib_path)/librclcpp.so | awk '{print $1}' | sed -e 's/.so.*//' | sed -e 's:/lib.*::'| sed -e 's/lib/-l/' | xargs echo)
@@ -47,7 +59,7 @@ ros_x86_libs := $(shell ls $(ros_lib_path)/x86_64-linux-gnu/lib*.so 2>/dev/null 
 # ros_libs := $(filter -l%,$(ros_libs))
 
 
-PATH_TO_WORKSPACE = /home/ubuntu/djnn_smala_install_debug/dev_ws
+PATH_TO_WORKSPACE = /home/lii/Documents/dev_ws
 icare_interfaces_libs_install_path := $(PATH_TO_WORKSPACE)/install/icare_interfaces/lib
 icare_libs := $(shell ls $(icare_interfaces_libs_install_path)/lib*.so 2>/dev/null | xargs echo)
 icare_libs := $(patsubst $(icare_interfaces_libs_install_path)/lib%.so, -l%, $(icare_libs))
