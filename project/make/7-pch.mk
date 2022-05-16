95dadb016f666037cd4d1d646854b29007d163da
stephane conversy <conversy@gmail.com>
2022-05-16 22:10:42 +0200

    make: reorganize pch

--- project/make/7-pch.mk
+++ project/make/7-pch.mk
@@ -7,26 +7,34 @@
 
 ifeq ($(compiler),llvm)
 pch_ext ?= .pch
-CXXFLAGS_PCH_GEN += -fpch-instantiate-templates -fpch-codegen -fpch-debuginfo
 endif
 ifeq ($(compiler),gnu)
 pch_ext ?= .gch
 endif
 
 pch_file ?= $(src_dir)/precompiled.hpp
-#pch_src ?= src/$(pch_file)
 pch_dst ?= $(build_dir)/$(pch_file)
+
+ifeq ($(compiler),llvm)
+CXXFLAGS_PCH_INC += -include-pch $(pch_dst)$(pch_ext)
+CXXFLAGS_PCH_GEN += -fpch-instantiate-templates -fpch-codegen -fpch-debuginfo
+pch_shared_dst := $(pch_dst:.hpp=.o)
+endif
+ifeq ($(compiler),gnu)
+# https://stackoverflow.com/a/3164874
+CXXFLAGS_PCH_INC += -I$(dir $(pch_dst)) -include $(notdir $(pch_file)) -Winvalid-pch
+#-fno-implicit-templates
 
 # SDL and other stuff define new variables for compiling, canceling the use of pch with gnu cc
 # FIXME this is not safe as every other external lib may define something
-ifeq ($(compiler),gnu)
 # https://gitlab.gnome.org/GNOME/gnome-online-accounts/-/merge_requests/14
 # Both GCC and Clang appear to expand -pthread to define _REENTRANT on their own
-CXXFLAGS_PCH_DEF += -D_REENTRANT
+# CXXFLAGS_PCH_DEF += -D_REENTRANT
 ifeq ($(display),SDL)
 CXXFLAGS_PCH_DEF += -Dmain=SDL_main
 endif
 endif
+
 
 $(build_dir)/%$(pch_ext): %
 	@mkdir -p $(dir $@)
@@ -37,17 +45,17 @@ else
 	@$(CXX) -x c++-header $(CXXFLAGS) $(CXXFLAGS_PCH_GEN) $< -o $@
 endif
 
-ifeq ($(compiler),llvm)
-CXXFLAGS_PCH_INC += -include-pch $(pch_dst)$(pch_ext)
-endif
-ifeq ($(compiler),gnu)
-# https://stackoverflow.com/a/3164874
-CXXFLAGS_PCH_INC += -I$(dir $(pch_dst)) -include $(notdir $(pch_file)) -Winvalid-pch
-#-fno-implicit-templates
-#$(build_dir)/src/core/utils/build/external_template.o: CXXFLAGS += -fimplicit-templates
-endif
-
 $(build_dir)/%$(pch_ext): override CXXFLAGS = $(CXXFLAGS_CFG) $(CXXFLAGS_PCH_DEF) $(djnn_cflags) $(CXXFLAGS_COMMON) $(CXXFLAGS_CK)
+
+# for llvm -fpch-instantiate-templates -fpch-codegen
+$(build_dir)/%precompiled.o: $(build_dir)/%precompiled.hpp$(pch_ext)
+ifeq ($V,max)
+	$(CXX) -c $< -o $@
+else
+	@$(call rule_message,compiling,$(stylized_target))
+	@$(CXX) -c $< -o $@
+	@printf "{\"directory\": \"$(root_dir)\", \"command\": \"$(CXX)  -c $< -o $@\", \"file\": \"$<\"}" > $(build_dir)/$*.cccmd.json
+endif
 
 pch: $(pch_dst)
 clean_pch:
@@ -55,3 +63,6 @@ clean_pch:
 
 
 $(objs): $(pch_dst)$(pch_ext)
+
+#$(exe) += $(pch_shared_dst)
+#$(exe): $(pch_shared_dst)
