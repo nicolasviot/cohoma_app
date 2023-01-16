5ed6ca6886d02bf661c5e943a626a6b4aea8b68f
Mathieu Poirier <mathieu.poirier@enac.fr>
2023-01-16 17:39:37 +0100

    tile_manager - fixed filename used for each layer - and moved code

--- src/cpp/tiles_manager.cpp
+++ src/cpp/tiles_manager.cpp
@@ -208,21 +208,10 @@ add_to_request_queue (__request& p) {
 }
 
 static void
-build_request (djnn::Process* tile, int z, int row, int col, const std::string& filepath, const std::string& layer_name)
+build_request (djnn::Process* tile, int z, int row, int col, const std::string& filepath, const std::string& uri)
 { 
   // proxy
   std::string proxy = ((djnn::AbstractProperty*)(tile->find_child("proxy")))->get_string_value();
-
-  // uri from layer_name
-  std::string uri = "UNKNOWN_LAYER";
-  if (layer_name.compare ("osm") == 0 )
-      uri = OSM_BASE_URI + std::to_string(z) + "/" + std::to_string(col) + "/" + std::to_string(row) + ".png";
-  else if (layer_name.compare ("geoportail") == 0 )
-      uri = GEOPORTAIL_BASE_URI + std::to_string(z) + "&TileRow=" + std::to_string(row) + "&TileCol=" + std::to_string(col);
-  else {
-    std::cerr << "ERROR - UNKNOWN layer \"" << layer_name << "\" -- should be: osm/geoportail" << std::endl;
-    return;
-  }
 
   __request t;
   t.tile = tile;
@@ -251,11 +240,25 @@ load_tiles_from(djnn::Process* src) {
 
   int max = pow(2, Z);
   if (X < max && Y < max) {
-    std::string new_path = "cache/" + layer_name + "/" + std::to_string(Z) + "_" + std::to_string(X) + "_" + std::to_string(Y) + ".png";
+    
+    std::string new_path = "cache/" + layer_name + "/" + std::to_string(Z) + "_" + std::to_string(X) + "_" + std::to_string(Y);
+    std::string uri = "UNKNOWN_LAYER";
+    if (layer_name.compare ("osm") == 0 ) {
+      uri = OSM_BASE_URI + std::to_string(Z) + "/" + std::to_string(X) + "/" + std::to_string(Y) + ".png";
+      new_path = new_path + ".png";
+    }
+    else if (layer_name.compare ("geoportail") == 0 ) {
+      uri = GEOPORTAIL_BASE_URI + std::to_string(Z) + "&TileRow=" + std::to_string(Y) + "&TileCol=" + std::to_string(X);
+      new_path = new_path + ".jpg";
+    }
+    else {
+      std::cerr << "ERROR - UNKNOWN layer \"" << layer_name << "\" -- should be: osm/geoportail" << std::endl;
+    return;
+    }
 
     // if file do not exist or empty
     if (!filesystem::exists(new_path) || std::filesystem::file_size(new_path) == 0) {
-      build_request (tile, Z, Y, X, new_path, layer_name);
+      build_request (tile, Z, Y, X, new_path, uri);
       // djnn::lock_ios_mutex();
       // std::cerr << new_path << " doesn't exist or is empty" << std::endl;
       // djnn::release_ios_mutex();
